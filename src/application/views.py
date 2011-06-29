94ab9a5af6ba8a47f1d1cf4c98a05a76892533dc
javi <qualopec@gmail.com>
2011-06-29 12:07:34 +0200

    simplifed url scheme

--- src/application/views.py
+++ src/application/views.py
@@ -1,14 +1,4 @@
-"""
-views.py
-
-URL route handlers
-
-Note that any handler params must match the URL route params.
-For example the *say_hello* handler, handling the URL route '/hello/<username>',
-  must be passed *username* as the argument.
-
-"""
-
+# encoding: utf-8
 
 from google.appengine.api import users
 from google.appengine.runtime.apiproxy_errors import CapabilityDisabledError
@@ -19,22 +9,27 @@ from models import ExampleModel
 from decorators import login_required, admin_required
 from forms import ExampleForm
 
+from application import app
 
+@app.route('/')
 def home():
     return redirect(url_for('list_examples'))
 
 
+@app.route('/hello/<username>')
 def say_hello(username):
     """Contrived example to demonstrate Flask's url routing capabilities"""
     return 'Hello %s' % username
 
 
+@app.route('/examples')
 def list_examples():
     """List all examples"""
     examples = ExampleModel.all()
     return render_template('list_examples.html', examples=examples)
 
 
+@app.route('/example/new', methods=['GET', 'POST'])
 @login_required
 def new_example():
     """Add a new example, detecting whether or not App Engine is in read-only mode."""
@@ -55,12 +50,14 @@ def new_example():
     return render_template('new_example.html', form=form)
 
 
+@app.route('/admin_only')
 @admin_required
 def admin_only():
     """This view requires an admin account"""
     return 'Super-seekrit admin page.'
 
 
+@app.route('/_ah/warmup')
 def warmup():
     """App Engine warmup handler
     See http://code.google.com/appengine/docs/python/config/appconfig.html#Warming_Requests
